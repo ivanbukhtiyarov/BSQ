cd61ed84c2bf001c96f2a0eac3cd393a1168cb3a
Lavonne Filius <lfilius@mi-u2.21-school.ru>
2019-08-21 10:57:12 +0300

    Add includehead.h and new firstline.c

---
+++ includehead.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   includehead.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lfilius <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/21 10:53:53 by lfilius           #+#    #+#             */
+/*   Updated: 2019/08/21 10:55:37 by lfilius          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INCLUDEHEAD_H
+# define INCLUDEHEAD_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#endif
