5e61d7a7097057c35e9fe78bab5d4cf9add3268b
Lavonne Filius <lfilius@mi-u2.21-school.ru>
2019-08-21 13:44:29 +0300

    add draw.c and includehead.h

---
+++ draw.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   draw.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lfilius <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/21 10:59:52 by lfilius           #+#    #+#             */
+/*   Updated: 2019/08/21 13:36:37 by lfilius          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includehead.h"
+#include "bsqhead.h"
+
+int		is_square(int *cord, int *symb, int i, int j)
+{
+	int i_loc;
+	int j_loc;
+
+	i_loc = cord[1] - cord[0] + 1;
+	j_loc = cord[2] - cord[0] + 1;
+	if ((i >= i_loc && i <= cord[1]) && (j >= j_loc && j <= cord[2]))
+		return (1);
+	else
+		return (0);
+}
+
+void	skip(int *fd)
+{
+	char buf[1];
+
+	while (buf[0] != '\n')
+		read(*fd, buf, 1);
+}
+
+void	draw(char **av, int *cord, int *symb, int file_num)
+{
+	int fl;
+	int buf[1];
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	fl = open(av[1], O_RDONLY);
+	if (fl == -1)
+		error();
+	skip(&fl);
+	while (read(fl, buf, 1))
+	{
+		if (!is_square(cord, symb, i, j))
+			write(1, &buf[0], 1);
+		else
+			write(1, &symb[3], 1);
+		if (buf[0] == '\n')
+		{
+			i++;
+			j = -1;
+		}
+		j++;
+	}
+	close(fl);
+}
--- includehead.h
+++ includehead.h
@@ -6,18 +6,18 @@
 /*   By: lfilius <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/21 10:53:53 by lfilius           #+#    #+#             */
-/*   Updated: 2019/08/21 10:55:37 by lfilius          ###   ########.fr       */
+/*   Updated: 2019/08/21 13:40:23 by lfilius          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef INCLUDEHEAD_H
 # define INCLUDEHEAD_H
 
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
 
 #endif
